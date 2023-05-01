14968608959ddf54f84715e1c1d3e21936df2f3d
Megan Payne <payneconsulting.nl@gmail.com>
2023-05-01 23:30:24 +0200

    cleaned up day 29 python file

--- 2023_challenge/image_hist_day29.py
+++ 2023_challenge/image_hist_day29.py
@@ -1,26 +1,27 @@
-import pandas as pd
-
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 
+# Get the image pixel data 
 imgpath = "snake_river_Tetons.TIF"
 img = cv2.imread(imgpath,0)
 
-fig, axs = plt.subplots(1, 2, figsize=(15, 15))
+
+# Plot the histogram on the left and grayscale of the image on the right.
+fig, axs = plt.subplots(1, 2, figsize=(15, 10))
+# Histogram subplot
+plt.subplot(1,2,1)
+plt.hist(img.ravel(), bins=255, color="dimgray", )
+plt.title('Grayscale pixel values for image on right')
+plt.ylabel("Pixel Count")
+plt.xlabel("Color value 0 to 255")
+
+# Grayscale image subplot.
 plt.subplot(1, 2, 2)
 plt.imshow(img, cmap='gray')
 plt.title('Ansel Adams - The Tetons & Snake River')
 plt.xticks([])
 plt.yticks([])
-plt.subplot(1,2,1)
-hist, bins = np.histogram(img.ravel(), 256, [0,255])
-# plt.xlim([0,255])
-plt.hist(img.ravel(), bins=255, color="dimgray", )
-plt.title('Grayscale pixel values for image on right')
-plt.ylabel("Pixel Count")
-plt.xlabel("Color value 0 to 255")
+
 txt = "Graphic: Megan Payne\nImage source: https://catalog.archives.gov/id/519904"
 fig.text(.5, .05, txt, ha='left')
-plt.show()
-
+plt.savefig("output/monocrome_hist_plot_day29.png")
